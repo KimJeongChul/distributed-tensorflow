a06e91fc45dc6849e5541d26f6d7a5449aba4223
KimJungChul <kjc5443@gmail.com>
2017-04-24 13:24:55 +0900

    Update and rename tf_distributed_ps.py to tf_distributed_1000Matrix.py

--- tf_distributed_1000Matrix.py
+++ tf_distributed_1000Matrix.py
@@ -37,52 +37,16 @@ elif FLAGS.job_name == "worker":
         # count the number of updates
         global_step = tf.get_variable('global_step', [], initializer = tf.constant_initializer(0), trainable = False)
 
-        # input images
-        with tf.name_scope('input'):
-          # None -> batch size can be any size, 784 -> flattened mnist image
-          x = tf.placeholder(tf.float32, shape=[None, 784], name="x-input")
-          # target 10 output classes
-          y_ = tf.placeholder(tf.float32, shape=[None, 10], name="y-input")
-
         # model parameters will change during training so we use tf.Variable
         tf.set_random_seed(1)
         with tf.name_scope("weights"):
-                W1 = tf.Variable(tf.random_normal([784, 100]))
-                W2 = tf.Variable(tf.random_normal([100, 10]))
+                A = tf.Variable(tf.random_normal([1000, 1000]))
+                B = tf.Variable(tf.random_normal([1000, 1000]))
 
-        # bias
-        with tf.name_scope("biases"):
-                b1 = tf.Variable(tf.zeros([100]))
-                b2 = tf.Variable(tf.zeros([10]))
-
-        # implement model
-        with tf.name_scope("softmax"):
-                # y is our prediction
-                z2 = tf.add(tf.matmul(x,W1),b1)
-                a2 = tf.nn.sigmoid(z2)
-                z3 = tf.add(tf.matmul(a2,W2),b2)
-                y  = tf.nn.softmax(z3)
-
-        # specify cost function
-        with tf.name_scope('cross_entropy'):
-                # this is our cost
-                cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), axis=[1]))
-
-        # specify optimizer
-        with tf.name_scope('train'):
-                # optimizer is an "operation" which we can execute in a session
-                grad_op = tf.train.GradientDescentOptimizer(learning_rate)
-                train_op = grad_op.minimize(cross_entropy, global_step=global_step)
-
-        with tf.name_scope('Accuracy'):
-            # accuracy
-            correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-        # create a summary for our cost and accuracy
-        tf.summary.scalar("cost", cross_entropy)
-        tf.summary.scalar("accuracy", accuracy)
-
+        # matrix multiplication
+        with tf.name_scope("matmul"):
+                C = tf.matmul(A,B)
+                
         # merge all summaries into a single "operation" which we can execute in a session
         summary_op = tf.summary.merge_all()
         init_op = tf.global_variables_initializer()
